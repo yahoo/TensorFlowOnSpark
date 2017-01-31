07ab6021d24fc0f38d7b5bb9986e624fb3e303d7
leewyang <leewyang@yahoo-inc.com>
2017-01-31 15:19:05 -0800

    fix syntax

--- src/com/yahoo/ml/tf/TFSparkNode.py
+++ src/com/yahoo/ml/tf/TFSparkNode.py
@@ -108,15 +108,16 @@ def reserve(cluster_spec, tensorboard, queues=['input', 'output']):
                 tb_sock.bind(('',0))
                 tb_port = tb_sock.getsockname()[1]
                 tb_sock.close()
-                logdir="tensorboard_%d" %(worker_num)
-                if os.environ.contains('PYSPARK_PYTHON'):
-                  # user-specified Python (typically --archives Python.zip)
+                logdir = "tensorboard_%d" %(worker_num)
+
+                if 'PYSPARK_PYTHON' in os.environ:
+                  # user-specified Python (typically Python.zip)
                   pypath = os.environ['PYSPARK_PYTHON']
-                  logging.info("PYSPARK_PYTHON: {0}".format(pypath)
+                  logging.info("PYSPARK_PYTHON: {0}".format(pypath))
                   pydir = os.path.dirname(pypath)
                   tb_proc = subprocess.Popen([pypath, "%s/tensorboard"%pydir, "--logdir=%s"%logdir, "--port=%d"%tb_port, "--debug"])
                 else:
-                  # system-installed Python & tensorboard in PATH
+                  # system-installed Python & tensorboard
                   tb_proc = subprocess.Popen(["tensorboard", "--logdir=%s"%logdir, "--port=%d"%tb_port, "--debug"])
 
             # find a free port for TF
