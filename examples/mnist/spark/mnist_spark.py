f4a2dd59266fb0cd862aef4287c22fe3f38a39ac
leewyang <leewyang@yahoo-inc.com>
2017-07-28 09:15:55 -0700

    move TFPipeline into tfos dir

--- examples/mnist/spark/mnist_spark.py
+++ examples/mnist/spark/mnist_spark.py
@@ -20,7 +20,7 @@ import time
 from datetime import datetime
 
 from tensorflowonspark import TFCluster
-from TFPipeline import TFEstimator, TFModel
+from tensorflowonspark.pipeline import TFEstimator, TFModel
 import mnist_dist
 
 sc = SparkContext(conf=SparkConf().setAppName("mnist_spark"))
