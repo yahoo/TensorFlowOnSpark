5c673e0ced4fb5f3e31f490bf15ac0ab246d47c9
Lee Yang <leewyang@gmail.com>
2017-09-26 13:31:07 -0700

    consolidate pipeline examples

--- examples/mnist/pipeline/spark/mnist_spark.py
+++ examples/mnist/pipeline/spark/mnist_spark.py
@@ -11,11 +11,12 @@ from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 
 import argparse
-import numpy
+import sys
 import tensorflow as tf
 from datetime import datetime
 
-from tensorflowonspark.pipeline import TFEstimator
+from tensorflowonspark import dfutil
+from tensorflowonspark.pipeline import TFEstimator, TFModel
 import mnist_dist
 
 sc = SparkContext(conf=SparkConf().setAppName("mnist_spark"))
@@ -27,7 +28,7 @@ num_ps = 1
 
 parser = argparse.ArgumentParser()
 
-######## PARAMS ########
+######## ARGS ########
 
 ## TFoS/cluster
 parser.add_argument("--batch_size", help="number of records per batch", type=int, default=100)
@@ -40,13 +41,16 @@ parser.add_argument("--protocol", help="Tensorflow network protocol (grpc|rdma)"
 parser.add_argument("--steps", help="maximum number of steps", type=int, default=1000)
 parser.add_argument("--tensorboard", help="launch tensorboard process", action="store_true")
 
-######## ARGS ########
-
 # Spark input/output
-parser.add_argument("--format", help="example format: (csv|pickle|tfr)", choices=["csv","pickle","tfr"], default="csv")
+parser.add_argument("--format", help="example format: (csv|tfr)", choices=["csv","tfr"], default="csv")
 parser.add_argument("--images", help="HDFS path to MNIST images in parallelized format")
 parser.add_argument("--labels", help="HDFS path to MNIST labels in parallelized format")
 parser.add_argument("--output", help="HDFS path to save test/inference output", default="predictions")
+
+# Execution Modes
+parser.add_argument("--train", help="train a model using Estimator", action="store_true")
+parser.add_argument("--inference_mode", help="type of inferencing (none|signature|direct|checkpoint)", choices=["none","signature","direct","checkpoint"], default="none")
+parser.add_argument("--inference_output", help="output of inferencing (predictions|features)", choices=["predictions","features"], default="predictions")
 
 args = parser.parse_args()
 print("args:",args)
@@ -54,81 +58,80 @@ print("args:",args)
 print("{0} ===== Start".format(datetime.now().isoformat()))
 
 if args.format == "tfr":
-  images = sc.newAPIHadoopFile(args.images, "org.tensorflow.hadoop.io.TFRecordFileInputFormat",
-                              keyClass="org.apache.hadoop.io.BytesWritable",
-                              valueClass="org.apache.hadoop.io.NullWritable")
-  def toNumpy(bytestr):
-    example = tf.train.Example()
-    example.ParseFromString(bytestr)
-    features = example.features.feature
-    image = numpy.array(features['image'].int64_list.value)
-    label = numpy.array(features['label'].int64_list.value)
-    return (image, label)
-  dataRDD = images.map(lambda x: toNumpy(str(x[0])))
-else:
-  if args.format == "csv":
-    images = sc.textFile(args.images).map(lambda ln: [int(x) for x in ln.split(',')])
-    labels = sc.textFile(args.labels).map(lambda ln: [float(x) for x in ln.split(',')])
-  else:  # args.format == "pickle":
-    images = sc.pickleFile(args.images)
-    labels = sc.pickleFile(args.labels)
-  print("zipping images and labels")
+  df = dfutil.loadTFRecords(sc, args.images)
+elif args.format == "csv":
+  images = sc.textFile(args.images).map(lambda ln: [int(x) for x in ln.split(',')])
+  labels = sc.textFile(args.labels).map(lambda ln: [float(x) for x in ln.split(',')])
   dataRDD = images.zip(labels)
+  df = spark.createDataFrame(dataRDD, ['image', 'label'])
+else:
+  raise Exception("Unsupported format: {}".format(args.format))
 
 # Pipeline API
-df = spark.createDataFrame(dataRDD, ['col1', 'col2'])
 
-print("{0} ===== Estimator.fit()".format(datetime.now().isoformat()))
-# dummy tf args (from imagenet/inception example)
-tf_args = { 'initial_learning_rate': 0.045, 'num_epochs_per_decay': 2.0, 'learning_rate_decay_factor': 0.94 }
-
-estimator = TFEstimator(mnist_dist.map_fun, tf_args) \
-        .setInputMapping({'col1':'image', 'col2':'label'}) \
-        .setModelDir(args.model_dir) \
+if args.train:
+  # train a model using Spark Estimator fitted to a DataFrame
+  print("{0} ===== Estimator.fit()".format(datetime.now().isoformat()))
+  # dummy tf args (from imagenet/inception example)
+  tf_args = { 'initial_learning_rate': 0.045, 'num_epochs_per_decay': 2.0, 'learning_rate_decay_factor': 0.94 }
+  estimator = TFEstimator(mnist_dist.map_fun, tf_args) \
+          .setInputMapping({'image':'image', 'label':'label'}) \
+          .setModelDir(args.model_dir) \
+          .setExportDir(args.export_dir) \
+          .setClusterSize(args.cluster_size) \
+          .setNumPS(args.num_ps) \
+          .setProtocol(args.protocol) \
+          .setTensorboard(args.tensorboard) \
+          .setEpochs(args.epochs) \
+          .setBatchSize(args.batch_size) \
+          .setSteps(args.steps)
+  model = estimator.fit(df)
+else:
+  # use a previously trained/exported model
+  model = TFModel(args) \
         .setExportDir(args.export_dir) \
-        .setClusterSize(args.cluster_size) \
-        .setNumPS(args.num_ps) \
-        .setProtocol(args.protocol) \
-        .setTensorboard(args.tensorboard) \
-        .setEpochs(args.epochs) \
-        .setBatchSize(args.batch_size) \
-        .setSteps(args.steps)
+        .setBatchSize(args.batch_size)
 
-model = estimator.fit(df)
+# NO INFERENCING
+if args.inference_mode == 'none':
+  sys.exit(0)
 
-#
-# Using exported signature defs w/ tensor aliases
-#
+# INFER FROM TENSORFLOW CHECKPOINT
+elif args.inference_mode == 'checkpoint':
+  model.setModelDir(args.model_dir)                         # load model from checkpoint at args.model_dir
+  model.setExportDir(None)
+  model.setInputMapping({'image':'x'})                      # map DataFrame 'image' column to the 'x' input tensor
+  if args.inference_output == 'predictions':
+    model.setOutputMapping({'prediction':'col_out'})        # map 'prediction' output tensor to output DataFrame 'col_out' column
+  else:  # args.inference_output == 'features':
+    model.setOutputMapping({'prediction':'col_out', 'Relu':'col_out2'})   # add 'Relu' output tensor to output DataFrame 'col_out2' column
 
-# prediction
-model.setTagSet(tf.saved_model.tag_constants.SERVING) \
-      .setSignatureDefKey(tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY) \
-      .setInputMapping({'col1':'image'}) \
-      .setOutputMapping({'prediction':'col_out'}) \
+# INFER USING TENSORFLOW SAVED_MODEL WITH EXPORTED SIGNATURES
+elif args.inference_mode == 'signature':
+  model.setModelDir(None)
+  model.setExportDir(args.export_dir)                       # load saved_model from args.export_dir
+  model.setTagSet(tf.saved_model.tag_constants.SERVING)     # using default SERVING tagset
+  model.setInputMapping({'image':'image'})                  # map DataFrame 'image' column to the 'image' input tensor alias of signature
+  if args.inference_output == 'predictions':
+    model.setSignatureDefKey(tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY)   # default signature def key, i.e. 'predict'
+    model.setOutputMapping({'prediction':'col_out'})        # map 'prediction' output tensor alias to output DataFrame 'col_out' column
+  else:  # args.inference_output == 'features'
+    model.setSignatureDefKey('featurize')                   # custom signature def key
+    model.setOutputMapping({'features':'col_out'})          # map 'features' output tensor alias to output DataFrame 'col_out' column
 
-# featurize
-# model.setTagSet(tf.saved_model.tag_constants.SERVING) \
-#      .setSignatureDefKey('featurize') \
-#      .setInputMapping({'col1':'image'}) \
-#      .setOutputMapping({'features':'col_out'})
-
-#
-# Using custom/direct mappings w/ tensors
-#
-
-# prediction
-# model.setTagSet(tf.saved_model.tag_constants.SERVING) \
-#       .setInputMapping({'col1':'x'}) \
-#       .setOutputMapping({'prediction':'col_out'})
-
-# featurize
-# model.setTagSet(tf.saved_model.tag_constants.SERVING) \
-#       .setInputMapping({'col1':'x'}) \
-#       .setOutputMapping({'prediction':'col_out1', 'Relu':'col_out2'})
+# INFER USING TENSORFLOW SAVED_MODEL, IGNORING EXPORTED SIGNATURES
+else:  # args.inference_mode == 'direct':
+  model.setModelDir(None)
+  model.setExportDir(args.export_dir)                       # load saved_model from args.export_dir
+  model.setTagSet(tf.saved_model.tag_constants.SERVING)     # using default SERVING tagset
+  model.setInputMapping({'image':'x'})                      # map DataFrame 'image' column to the 'x' input tensor
+  if args.inference_output == 'predictions':
+    model.setOutputMapping({'prediction': 'col_out'})       # map 'prediction' output tensor to output DataFrame 'col_out' column
+  else:  # args.inference_output == 'features'
+    model.setOutputMapping({'prediction': 'col_out', 'Relu': 'col_out2'})   # add 'Relu' output tensor to output DataFrame 'col_out2' column
 
 print("{0} ===== Model.transform()".format(datetime.now().isoformat()))
 preds = model.transform(df)
 preds.write.json(args.output)
 
 print("{0} ===== Stop".format(datetime.now().isoformat()))
-
--- examples/mnist/pipeline/tf/mnist_spark.py
+++ examples/mnist/pipeline/tf/mnist_spark.py
@@ -11,10 +11,12 @@ from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 
 import argparse
+import sys
+import tensorflow as tf
 from datetime import datetime
 
 from tensorflowonspark import TFCluster, dfutil
-from tensorflowonspark.pipeline import TFEstimator
+from tensorflowonspark.pipeline import TFEstimator, TFModel
 import mnist_dist
 
 sc = SparkContext(conf=SparkConf().setAppName("mnist_tf"))
@@ -26,7 +28,7 @@ num_ps = 1
 
 parser = argparse.ArgumentParser()
 
-######## PARAMS ########
+######## ARGS ########
 
 ## TFoS/cluster
 parser.add_argument("--batch_size", help="number of records per batch", type=int, default=100)
@@ -41,16 +43,21 @@ parser.add_argument("--readers", help="number of reader/enqueue threads", type=i
 parser.add_argument("--steps", help="maximum number of steps", type=int, default=1000)
 parser.add_argument("--tensorboard", help="launch tensorboard process", action="store_true")
 
-######## ARGS ########
-
 # Spark input/output
 parser.add_argument("--format", help="example format: (csv|tfr)", choices=["csv","tfr"], default="csv")
 parser.add_argument("--images", help="HDFS path to MNIST images in parallelized format")
 parser.add_argument("--labels", help="HDFS path to MNIST labels in parallelized format")
 parser.add_argument("--output", help="HDFS path to save test/inference output", default="predictions")
 
+# Execution Modes
+parser.add_argument("--train", help="train a model using Estimator", action="store_true")
+parser.add_argument("--inference_mode", help="type of inferencing (none|signature|direct)", choices=["none","signature","direct"], default="none")
+parser.add_argument("--inference_output", help="output of inferencing (predictions|features)", choices=["predictions","features"], default="predictions")
+
 args = parser.parse_args()
 print("args:",args)
+
+print("{0} ===== Start".format(datetime.now().isoformat()))
 
 if args.format == "tfr":
   df = dfutil.loadTFRecords(sc, args.images)
@@ -62,23 +69,64 @@ elif args.format == "csv":
 else:
   raise Exception("Unsupported format: {}".format(args.format))
 
-print("{0} ===== Start".format(datetime.now().isoformat()))
-estimator = TFEstimator(mnist_dist.map_fun, args, export_fn=mnist_dist.export_fun) \
-        .setModelDir(args.model_dir) \
-        .setExportDir(args.export_dir) \
-        .setClusterSize(args.cluster_size) \
-        .setNumPS(args.num_ps) \
-        .setInputMode(TFCluster.InputMode.TENSORFLOW) \
-        .setTFRecordDir(args.tfrecord_dir) \
-        .setProtocol(args.protocol) \
-        .setReaders(args.readers) \
-        .setTensorboard(args.tensorboard) \
-        .setEpochs(args.epochs) \
-        .setBatchSize(args.batch_size) \
-        .setSteps(args.steps)
+# Pipeline API
 
-tf_args = { 'initial_learning_rate': 0.01 }
-model = estimator.fit(df)
+if args.train:
+  # train a model using Spark Estimator fitted to a DataFrame
+  print("{0} ===== Estimator.fit()".format(datetime.now().isoformat()))
+  # dummy tf args (from imagenet/inception example)
+  tf_args = { 'initial_learning_rate': 0.045, 'num_epochs_per_decay': 2.0, 'learning_rate_decay_factor': 0.94 }
+  estimator = TFEstimator(mnist_dist.map_fun, args, export_fn=mnist_dist.export_fun) \
+          .setModelDir(args.model_dir) \
+          .setExportDir(args.export_dir) \
+          .setClusterSize(args.cluster_size) \
+          .setNumPS(args.num_ps) \
+          .setInputMode(TFCluster.InputMode.TENSORFLOW) \
+          .setTFRecordDir(args.tfrecord_dir) \
+          .setProtocol(args.protocol) \
+          .setReaders(args.readers) \
+          .setTensorboard(args.tensorboard) \
+          .setEpochs(args.epochs) \
+          .setBatchSize(args.batch_size) \
+          .setSteps(args.steps)
+  model = estimator.fit(df)
+else:
+  # use a previously trained/exported model
+  model = TFModel(args) \
+        .setExportDir(args.export_dir) \
+        .setBatchSize(args.batch_size)
+
+# NO INFERENCING
+if args.inference_mode == 'none':
+  sys.exit(0)
+
+# INFER USING EXPORTED SIGNATURES OF TENSORFLOW SAVED_MODEL
+elif args.inference_mode == 'signature':
+  model.setModelDir(None)
+  model.setExportDir(args.export_dir)                       # load saved_model from args.export_dir
+  model.setTagSet(tf.saved_model.tag_constants.SERVING)     # using default SERVING tagset
+  model.setInputMapping({'image':'image'})                  # map DataFrame 'image' column to the 'image' input tensor alias of signature
+  if args.inference_output == 'predictions':
+    model.setSignatureDefKey(tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY)   # default signature def key, i.e. 'predict'
+    model.setOutputMapping({'prediction':'col_out'})        # map 'prediction' output tensor alias to output DataFrame 'col_out' column
+  else:  # args.inference_output == 'features'
+    model.setSignatureDefKey('featurize')                   # custom signature def key
+    model.setOutputMapping({'features':'col_out'})          # map 'features' output tensor alias to output DataFrame 'col_out' column
+
+# INFER USING TENSORFLOW SAVED_MODEL, IGNORING EXPORTED SIGNATURES
+else:  # args.inference_mode == 'direct':
+  model.setModelDir(None)
+  model.setExportDir(args.export_dir)                       # load saved_model from args.export_dir
+  model.setTagSet(tf.saved_model.tag_constants.SERVING)     # using default SERVING tagset
+  model.setInputMapping({'image':'x'})                      # map DataFrame 'image' column to the 'x' input tensor
+  if args.inference_output == 'predictions':
+    model.setOutputMapping({'prediction': 'col_out'})       # map 'prediction' output tensor to output DataFrame 'col_out' column
+  else:  # args.inference_output == 'features'
+    model.setOutputMapping({'prediction': 'col_out', 'Relu': 'col_out2'})   # add 'Relu' output tensor to output DataFrame 'col_out2' column
+
+print("{0} ===== Model.transform()".format(datetime.now().isoformat()))
+preds = model.transform(df)
+preds.write.json(args.output)
 
 print("{0} ===== Stop".format(datetime.now().isoformat()))
 
