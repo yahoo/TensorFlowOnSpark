24154f94ec98ed8c772043010f2730e7d036bdb3
Lee Yang <leewyang@gmail.com>
2018-09-08 07:00:48 -0700

    try autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+_pysrc = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', '..'))
+sys.path.insert(0, _pysrc)
+
+autodoc_mock_imports = ["pyspark", "tensorflow"]
 
 # -- Project information -----------------------------------------------------
 
