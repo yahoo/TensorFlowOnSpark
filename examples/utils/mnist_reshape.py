d57c3c0907787036cd2bb6485f9cf7fe9fd76000
Lee Yang <leewyang@verizonmedia.com>
2019-08-15 13:36:43 -0700

    move model_export.py into examples/util

--- examples/utils/mnist_reshape.py
+++ examples/utils/mnist_reshape.py
@@ -1,3 +1,7 @@
+# Copyright 2019 Yahoo Inc.
+# Licensed under the terms of the Apache 2.0 license.
+# Please see LICENSE file in the project root for terms.
+
 import sys
 import numpy as np
 vec = [int(x) for x in next(sys.stdin).split(',')]
