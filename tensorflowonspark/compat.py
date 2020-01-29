04bb364245bb0f83f569abb3dad11450c17d35e2
Lee Yang <leewyang@verizonmedia.com>
2020-01-29 14:30:16 -0800

    adjust conditional ordering

--- tensorflowonspark/compat.py
+++ tensorflowonspark/compat.py
@@ -8,17 +8,17 @@ from packaging import version
 
 
 def export_saved_model(model, export_dir, is_chief=False):
-  if version.parse(tf.__version__) == version.parse('2.1.0'):
+  if version.parse(tf.__version__) < version.parse('2.1.0'):
+    if is_chief:
+      tf.keras.experimental.export_saved_model(model, export_dir)
+  else:
     # non-chief nodes save to dummy location on local disk
     export_dir = export_dir if is_chief else 'worker_model'
     model.save(export_dir, save_format='tf')
-  else:  # if version.parse(tf.__version__) <= version.parse('2.0.0'):
-    if is_chief:
-      tf.keras.experimental.export_saved_model(model, export_dir)
 
 
 def disable_auto_shard(options):
-  if version.parse(tf.__version__) == version.parse('2.0.0'):
+  if version.parse(tf.__version__) < version.parse('2.1.0'):
     options.experimental_distribute.auto_shard = False
   else:
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
