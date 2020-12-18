6f042b4e7d4d84bf36475e0b5020ed8d2cf462ed
Lee Yang <leewyang@verizonmedia.com>
2020-12-18 08:50:22 -0800

    migrate build from travis to screwdriver

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,9 +28,9 @@ copyright = '2020, Yahoo Inc / Verizon Media'
 author = 'Yahoo Inc'
 
 # The short X.Y version
-version = '2.2.1'
+version = '2.2.2'
 # The full version, including alpha/beta/rc tags
-release = '2.2.1'
+release = '2.2.2'
 
 
 # -- General configuration ---------------------------------------------------
---
+++ scripts/start_spark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+#export SPARK_HOME=/opt/spark
+#export SPARK_LOCAL_IP=127.0.0.1
+#export PATH=$SPARK_HOME/bin:$PATH
+#
+## Start Spark Standalone Cluster
+#export SPARK_CLASSPATH=./lib/tensorflow-hadoop-1.0-SNAPSHOT.jar
+#export MASTER=spark://$(hostname):7077
+#export SPARK_WORKER_INSTANCES=2; export CORES_PER_WORKER=1
+#export TOTAL_CORES=$((${CORES_PER_WORKER}*${SPARK_WORKER_INSTANCES}))
+
+${SPARK_HOME}/sbin/start-master.sh; ${SPARK_HOME}/sbin/start-slave.sh -c ${CORES_PER_WORKER} -m 1G ${MASTER}
