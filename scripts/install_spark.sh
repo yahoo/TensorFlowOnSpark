db541a74e509b58d25d8c499f4d27db5f476a4ba
Lee Yang <leewyang@verizonmedia.com>
2021-01-26 22:13:27 +0000

    update python build for spark 3 dependencies

--- scripts/install_spark.sh
+++ scripts/install_spark.sh
@@ -5,7 +5,10 @@ yum install -y java-1.8.0-openjdk
 export JAVA_HOME=/usr/lib/jvm/jre-1.8.0
 
 # Install Spark
-export SPARK_VERSION=2.4.7
-curl -LO http://www-us.apache.org/dist/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop2.7.tgz
+export SPARK_VERSION=3.0.1
+export HADOOP_VERSION=2.7
+curl -LO https://downloads.apache.org/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz
+
+export SPARK_HOME=/opt/spark
 mkdir $SPARK_HOME
-tar -xf spark-${SPARK_VERSION}-bin-hadoop2.7.tgz -C $SPARK_HOME --strip-components=1
+tar -xf spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz -C $SPARK_HOME --strip-components=1
