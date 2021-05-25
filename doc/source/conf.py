3ee921a3093886702280de5b191d093de8c3d524
Lee Yang <leewyang@verizonmedia.com>
2021-05-25 11:18:43 -0700

    v2.2.4 release

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,9 +28,9 @@ copyright = '2020, Yahoo Inc / Verizon Media'
 author = 'Yahoo Inc'
 
 # The short X.Y version
-version = '2.2.3'
+version = '2.2.4'
 # The full version, including alpha/beta/rc tags
-release = '2.2.3'
+release = '2.2.4'
 
 
 # -- General configuration ---------------------------------------------------
